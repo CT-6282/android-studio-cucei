99c0f1331f4ffe671b4c8811ca40b81d8e0f8a6f
CT-6282 <Godric@MacBook-Pro.local>
2023-12-01 17:17:19 -0600

    1d

--- AwsomeProject/Tab4.js
+++ AwsomeProject/Tab4.js
@@ -1,7 +1,11 @@
 import React, { Component } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import MapViewDirections from 'react-native-maps-directions';
 
+const origin = {latitude: 20.654961472970044, longitude: -103.32535314445454};
+const destination = {latitude: 20.71148054871335, longitude: -103.4121945519942};
+const GOOGLE_MAPS_APIKEY = 'AIzaSyBKA4LC02aLcccvwgU0XdOhe_BzImQBjz4';
 export default class Tab4 extends Component {
   constructor(props) {
     super(props);
@@ -22,6 +26,17 @@ export default class Tab4 extends Component {
             longitudeDelta: 0.0121,
           }}
         >
+          <MapViewDirections
+            origin={origin}
+            destination={destination}
+            strokeWidth={5}
+            strokeColor="red"
+            apikey={GOOGLE_MAPS_APIKEY}
+            onReady={result => {
+              console.log("La distancia entre CUCEI y Andares es " + result.distance + " km")
+              console.log("La duración del recorrido será aprox " + result.duration + " min")
+            }}
+          />
         </MapView>
       </View>
     );
